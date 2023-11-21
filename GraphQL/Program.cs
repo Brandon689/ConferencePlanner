e9822ad9799a8e284e98a7bb546168914813408b
Brandon <brandon139@protonmail.com>
2023-11-21 19:41:14 +1100

    parallel execution, data, loader, expand models

--- GraphQL/Program.cs
+++ GraphQL/Program.cs
@@ -1,14 +1,19 @@
 using ConferencePlanner.GraphQL;
 using ConferencePlanner.GraphQL.Data;
+using ConferencePlanner.GraphQL.DataLoader;
 using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
-builder.Services.AddDbContext<ApplicationDbContext>(options => options.UseSqlite("Data Source=conferences.db"));
+builder.Services.AddPooledDbContextFactory<ApplicationDbContext>(options => options.UseSqlite("Data Source=conferences.db"));
+//builder.Services.AddPooledDbContextFactory<ApplicationDbContext>(options => options.UseSqlite("Data Source=conferences.db"));
+
 builder.Services
     .AddGraphQLServer()
+    //.RegisterDbContext<ApplicationDbContext>(DbContextKind.Pooled)
     .AddQueryType<Query>()
-    .AddMutationType<Mutation>();
+    .AddMutationType<Mutation>()
+    .AddDataLoader<SpeakerByIdDataLoader>();
 
 var app = builder.Build();
 
